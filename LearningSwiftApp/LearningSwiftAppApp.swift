3612d65ceee354e642630c7a534e80416b6f8513
Thore Brehmer <thore.brehmer@fu-berlin.de>
2023-08-10 19:42:34 +0200

    Initial Commit

---
+++ LearningSwiftApp/LearningSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftAppApp.swift
+//  LearningSwiftApp
+//
+//  Created by Thore Brehmer on 10.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningSwiftAppUITests/LearningSwiftAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LearningSwiftAppUITestsLaunchTests.swift
+//  LearningSwiftAppUITests
+//
+//  Created by Thore Brehmer on 10.08.23.
+//
+
+import XCTest
+
+final class LearningSwiftAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
